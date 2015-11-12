401507d9a52b0e6177e5665fb2f25ef3a744cf00
sk1go <gjdnbfc151>
2015-11-12 16:41:16 +0300

    Host project added

---
+++ HDDInfo/GetHddIngoHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHddIngoHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var host = new ServiceHost(typeof(HDDInfo.HddInfo)))
+            {
+                host.Open();
+                Console.WriteLine("Host is running...");
+                Console.ReadLine();
+                host.Close();
+            }
+
+           
+        }
+    }
+}
