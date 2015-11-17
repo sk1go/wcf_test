acb8ab2a13bd5f3ca1457f169ff4952c608f20e0
sk1go <gjdnbfc151>
2015-11-17 16:09:56 +0300

    working windows auth

--- HDDInfo/GetHddIngoHost/Program.cs
+++ HDDInfo/GetHddIngoHost/Program.cs
@@ -17,7 +17,6 @@ namespace GetHddIngoHost
                 host.Open();
                 Console.WriteLine("Host is running...");
                 Console.ReadLine();
-                host.Close();
             }
 
            
