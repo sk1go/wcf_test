81490f5bf182009c7b0c1dc71e5e036821080fd5
sk1go <gjdnbfc151>
2015-11-17 15:21:00 +0300

    Basic work with service

--- WPF_Client/WPF_Client/MainWindow.xaml.cs
+++ WPF_Client/WPF_Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel;
 
 namespace WPF_Client
 {
@@ -23,6 +24,47 @@ namespace WPF_Client
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var IP = "169.254.72.217";
+            //var Port = "12345";
+            TB_ip_1.Text = "169";
+            TB_ip_2.Text = "254";
+            TB_ip_3.Text = "72";
+            TB_ip_4.Text = "217";
+            TB_port.Text = "12345";
+
+            var IP = string.Format("{0}.{1}.{2}.{3}", TB_ip_1.Text, TB_ip_2.Text, TB_ip_3.Text, TB_ip_4.Text);
+            var Port = TB_port.Text;
+
+            var client = new ServiceReference1.HddInfoClient("NetTcpBinding_IHddInfo");
+            client.Endpoint.Address = new EndpointAddress(new Uri("net.tcp://" + IP + ":" + Port + "/HddInfo"));
+
+            client.ClientCredentials.UserName.UserName = "test_user";
+            client.ClientCredentials.UserName.Password = "1234";
+            try
+            {
+                var res3 = client.GetHddInfo();
+                foreach (var item in res3)
+                {
+                    LB_result.Items.Add("===================================");
+                    LB_result.Items.Add(item.Caption);
+                    LB_result.Items.Add(item.DeviceID);
+                    LB_result.Items.Add(item.Status);
+                    LB_result.Items.Add(item.Size);
+                    foreach (var ld in item.LogicalDrives)
+                    {
+                        LB_result.Items.Add(ld);
+                    }
+                    LB_result.Items.Add("===================================");
+                }
+            }
+            catch (Exception ex)
+            {
+                LB_result.Items.Add(ex.Message);
+            }
         }
     }
 }
